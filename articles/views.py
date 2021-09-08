7a02ee99af410140af73f4daa59d547781b4e1ba
Ajay <ap221882@gmail.com>
2021-09-08 13:31:39 +0530

    Added searchById and formToCreate

--- articles/views.py
+++ articles/views.py
@@ -1,3 +1,4 @@
+from django.db.models import query
 from django.shortcuts import render
 from .models import Article
 
@@ -10,3 +11,36 @@ def article_detail_view(request, id=None):
         "object":article_obj,
     }
     return render(request, "articles/detail.html", context=context) #request is used afterwards in templates
+
+# @csrf_exempt
+def article_create_view(request, id=None):
+    context = {}
+    if request.method == 'POST':
+        # print(request.POST)
+        title = request.POST.get('title')
+        content = request.POST.get('content')
+        print(title,content)
+        article_object = Article.objects.create(title=title,content=content)
+        context['object'] = article_object
+        # context['title'] = title
+        # context['content'] = content
+        context['created'] = True
+    return render(request, "articles/create.html", context=context)
+
+
+def article_search_view(request):
+    # print(dir(request))
+    print(request.GET)
+    query_dict = request.GET # this is a dictionary
+    # query = query_dict.get('q')
+    try:
+        query = int(query_dict.get('q'))
+    except:
+        query = None
+    article_obj = None
+    if query is not None:
+        article_obj = Article.objects.get(id=query)
+    context = {
+        "object":article_obj,
+    }
+    return render(request,"articles/search.html", context=context)
