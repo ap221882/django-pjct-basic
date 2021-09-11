235d8353479a4d1a07164ccc72258a7cf34e6a92
Ajay <ap221882@gmail.com>
2021-09-11 11:46:34 +0530

    Added Django loginForm

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 
 def register_view(request):
@@ -14,16 +14,17 @@ def register_view(request):
 # Create your views here.
 def login_view(request):
     if request.method == 'POST':
-        username = request.POST.get("username")
-        password = request.POST.get("password")
-        # print(username,password) # remove this!!!
-        user = authenticate(request, username = username, password=password)
-        if user is None:
-            context = {'error':'Invalid username or password.'}
-            return render(request,"accounts/login.html",context)
-        login(request,user)
-        return redirect('/')
-    return render(request,"accounts/login.html",{})
+        form = AuthenticationForm(request, data= request.POST)
+        if form.is_valid():
+            user=form.get_user()
+            login(request,user)
+            return redirect('/')
+    else:
+        form = AuthenticationForm(request)
+    context = {
+        'form':form
+    }
+    return render(request,"accounts/login.html",context)
 
 def logout_view(request):
     if request.method == "POST":
